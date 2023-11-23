b29c0e5a4e2ead0505c8182da8c4a378c1277f5c
Augusto Dantas <augustow.dantas@gmail.com>
2023-11-23 15:17:41 -0300

    Página de cadastro - Estrutura, CSS e usuário

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 // components
 import Navbar from './components/Navbar';
@@ -21,6 +23,8 @@ function App() {
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/About" element={<About />} />
+                        <Route path="/Login" element={<Login />} />
+                        <Route path="Register" element={<Register />} />
                     </Routes>
                 </div>
                 <Footer />
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+import styles from "./Login.module.css";
+
+import React from 'react'
+
+const Login = () => {
+
+
+
+  return (
+    <div>
+        <h1>Login</h1>
+    </div>
+  )
+}
+
+export default Login;
